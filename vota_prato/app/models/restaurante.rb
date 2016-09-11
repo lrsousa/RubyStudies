72f421bf0f841903ad4eaa59b5162c5e989dbe5c
rsleonardo <leonarrangel@gmail.com>
2016-09-11 00:15:09 -0300

    teminado capitulo 7.15

--- vota_prato/app/models/restaurante.rb
+++ vota_prato/app/models/restaurante.rb
@@ -1,2 +1,15 @@
 class Restaurante < ActiveRecord::Base
+	validates_presence_of :nome, message: "Deve ser preenchido"
+	validates_presence_of :endereco, message: "Deve ser preenchido"
+	validates_presence_of :especialidade, message: "Deve ser preenchido"
+
+	validates_uniqueness_of :nome, message: "Nome já cadastrado"
+	validates_uniqueness_of :endereco, message: "Endereco já cadastrado"
+
+	validate :primeira_letra_deve_ser_maiuscula
+
+	private
+	def primeira_letra_deve_ser_maiuscula
+		errors.add(:nome, "Primeira letra deve ser maiuscula") unless nome =~ /[A-Z].*/
+	end
 end
